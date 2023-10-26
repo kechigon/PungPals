654bd6c267b84611d39abc80bef97a2500bd1e57
Keigo Kurita <keig0xurita@gmail.com>
2023-10-26 05:16:07 +0000

    update modelse.py

--- PungPals/PunPals/models.py
+++ PungPals/PunPals/models.py
@@ -3,22 +3,59 @@ from django.db import models
 # Create your models here.
 class User(models.Model):
     name = models.CharField(max_length=100, primary_key=True)
-    rooms = models.ForeignKey(Room,on_delete=models.CASCADE)
+    passwd = models.CharField(max_length=100)
 
 class Room(models.Model):
-
-class RoomMid(models.Model):
-
-class UsersInRoom(models.Model):
+    roomName = models.CharField(max_length=100)
+    passwd = models.CharField(max_length=100)
+    users = models.ManyToManyField(User)
 
 class Taikyoku3(models.Model):
-
-class Taikyoku3Mid(models.Model):
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    userTon = models.ForeignKey(User, on_delete=models.CASCADE, related_name="taikyoku3_userTon")
+    userNan = models.ForeignKey(User, on_delete=models.CASCADE, related_name="taikyoku3_userNan")
+    userSya = models.ForeignKey(User, on_delete=models.CASCADE, related_name="tiakyoku3_userSya")
+    tonScore = models.IntegerField()
+    nanScore = models.IntegerField()
+    syaScore = models.IntegerField()
 
 class Taikyoku4(models.Model):
-
-class Taikyoku4Mid(models.Model):
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    userTon = models.ForeignKey(User, on_delete=models.CASCADE, related_name="taikyoku4_userTon")
+    userNan = models.ForeignKey(User, on_delete=models.CASCADE, related_name="taikyoku4_userNan")
+    userSya = models.ForeignKey(User, on_delete=models.CASCADE, related_name="taikyoku4_userSya")
+    userPe = models.ForeignKey(User, on_delete=models.CASCADE, related_name="taikyoku4_userPei")
+    tonScore = models.IntegerField()
+    NanScore = models.IntegerField()
+    SyaScore = models.IntegerField()
+    PeScore = models.IntegerField()
 
 class Senseki3(models.Model):
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rankMean = models.FloatField()
+    firstNum = models.IntegerField()
+    secondNum = models.IntegerField()
+    thirdNum = models.IntegerField()
+    firstRate = models.FloatField()
+    secondRate = models.FloatField()
+    thirdRate = models.FloatField()
+    outRate = models.FloatField()
+    gameNum = models.IntegerField()
+    scoreMean = models.FloatField()
 
 class Senseki4(models.Model):
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rankMean = models.FloatField()
+    firstNum = models.IntegerField()
+    secondNum = models.IntegerField()
+    thirdNum = models.IntegerField()
+    fourthNum = models.IntegerField()
+    firstRate = models.FloatField()
+    secondRate = models.FloatField()
+    thirdRate = models.FloatField()
+    fourthRate = models.FloatField()
+    outRate = models.FloatField()
+    gameNum = models.IntegerField()
+    scoreMean = models.FloatField()
--- PungPals/PungPals/settings.py
+++ PungPals/PungPals/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'PunPals.apps.PunpalsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
