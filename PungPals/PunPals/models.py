461f4bb716abac63077c256be0aec2bba2442849
Keigo Kurita <keig0xurita@gmail.com>
2023-10-29 13:20:24 +0900

    update models.py

--- PungPals/PunPals/models.py
+++ PungPals/PunPals/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 
-
 # Create your models here.
 class User(models.Model):
     name = models.CharField(max_length=100, primary_key=True)
     passwd = models.CharField(max_length=100)
+
+    def joindRoomNum(self):
+        return len(self.room_set.all())
 
     def __str__(self):
         return self.name
@@ -13,6 +15,9 @@ class Room(models.Model):
     roomName = models.CharField(max_length=100)
     passwd = models.CharField(max_length=100)
     users = models.ManyToManyField(User)
+
+    def usersNum(self):
+        return len(self.users.all())
 
     def __str__(self):
         return self.roomName
@@ -26,6 +31,14 @@ class Taikyoku3(models.Model):
     nanScore = models.IntegerField()
     syaScore = models.IntegerField()
 
+    def ranking(self):
+        score_list =  [(self.userTon, self.tonScore, 3),
+                       (self.userNan, self.nanScore, 2),
+                       (self.userSya, self.syaScore, 1)]
+        # sort by score and then by user rank in descending order
+        score_list.sort(key = lambda x: (x[1], x[2]), reverse=True)
+        return [user.name for user, _, _ in score_list]
+
 class Taikyoku4(models.Model):
     room = models.ForeignKey(Room, on_delete=models.CASCADE)
     userTon = models.ForeignKey(User, on_delete=models.CASCADE, related_name="taikyoku4_userTon")
@@ -33,17 +46,27 @@ class Taikyoku4(models.Model):
     userSya = models.ForeignKey(User, on_delete=models.CASCADE, related_name="taikyoku4_userSya")
     userPe = models.ForeignKey(User, on_delete=models.CASCADE, related_name="taikyoku4_userPei")
     tonScore = models.IntegerField()
-    NanScore = models.IntegerField()
-    SyaScore = models.IntegerField()
-    PeScore = models.IntegerField() 
+    nanScore = models.IntegerField()
+    syaScore = models.IntegerField()
+    peScore = models.IntegerField()
+
+    def ranking(self):
+        score_list =  [(self.userTon, self.tonScore, 4),
+                       (self.userNan, self.nanScore, 3),
+                       (self.userSya, self.syaScore, 2),
+                       (self.userPe, self.peScore, 1)]
+        # sort by score and then by user rank in descending order
+        score_list.sort(key = lambda x: (x[1], x[2]), reverse=True)
+        return [user.name for user, _, _ in score_list]
 
 class Senseki3(models.Model):
     room = models.ForeignKey(Room, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     rankMean = models.FloatField()
-    firstNum = models.IntegerField()
-    secondNum = models.IntegerField()
-    thirdNum = models.IntegerField()
+    firstNum = models.IntegerField(default=0)
+    secondNum = models.IntegerField(default=0)
+    thirdNum = models.IntegerField(default=0)
+    outNum = models.IntegerField(default=0)
     firstRate = models.FloatField()
     secondRate = models.FloatField()
     thirdRate = models.FloatField()
@@ -55,10 +78,11 @@ class Senseki4(models.Model):
     room = models.ForeignKey(Room, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     rankMean = models.FloatField()
-    firstNum = models.IntegerField()
-    secondNum = models.IntegerField()
-    thirdNum = models.IntegerField()
-    fourthNum = models.IntegerField()
+    firstNum = models.IntegerField(default=0)
+    secondNum = models.IntegerField(default=0)
+    thirdNum = models.IntegerField(default=0)
+    fourthNum = models.IntegerField(default=0)
+    outNum = models.IntegerField(default=0)
     firstRate = models.FloatField()
     secondRate = models.FloatField()
     thirdRate = models.FloatField()
