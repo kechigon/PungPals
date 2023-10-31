1c54e3eb506663b99b051175e359b7c9a64a35e6
Keigo Kurita <keig0xurita@gmail.com>
2023-10-31 12:07:16 +0900

    update urls.py views.py add home.html

--- PungPals/PunPals/views.py
+++ PungPals/PunPals/views.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse
+from django.views.generic import TemplateView
+from django.shortcuts import render
 
+from .models import User, Room, Taikyoku3, Taikyoku4, Senseki3, Senseki4
 
-def home(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+class Home(TemplateView):
+    template_name = "PunPals/home.html"
--- PungPals/PungPals/urls.py
+++ PungPals/PungPals/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("PunPals/", include("PunPals.urls")),
+    path("PungPals/", include("PunPals.urls")),
     path('admin/', admin.site.urls),
 ]
