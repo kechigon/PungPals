0ff23f6c8c9c238b77fba252c6ace66603976525
Keigo Kurita <keig0xurita@gmail.com>
2023-11-01 16:02:37 +0900

    update someting

--- PungPals/PunPals/urls.py
+++ PungPals/PunPals/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.Home.as_view(), name="home"),
+    path("signup", views.SignUp.as_view(), name="signup"),
+    path("user_home", views.UserHome.as_view(), name="user_home"),
 ]
--- PungPals/PunPals/views.py
+++ PungPals/PunPals/views.py
@@ -1,8 +1,13 @@
 from django.http import HttpResponse
-from django.views.generic import TemplateView
-from django.shortcuts import render
+from django.views.generic import TemplateView, ListView, CreateView
 
 from .models import User, Room, Taikyoku3, Taikyoku4, Senseki3, Senseki4
 
 class Home(TemplateView):
     template_name = "PunPals/home.html"
+
+class SignUp(CreateView):
+    template_name = "PunPals/signup.html"
+
+class UserHome(ListView):
+    template_name = "PunPals/user_home.html"
