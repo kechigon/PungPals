1d5abe5f3e879d05a6a7a6376b100cf6ef819420
Keigo Kurita <keig0xurita@gmail.com>
2024-01-21 20:28:30 +0900

    add terraform

---
+++ terraform/provider.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.51.0"
+    }
+  }
+}
+
+provider "google" {
+credentials = file(var.credentials_file)
+
+region  = var.region
+zone    = var.zone
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "credentials_file" {
+    description = "Path to crdential file"
+}
+
+variable "region" {
+  default = "asia-northeast1"
+}
+
+variable "zone" {
+  default = "asia-northeast1-b"
+}
